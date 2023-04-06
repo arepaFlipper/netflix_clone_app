d17d41b8820e62b10fdbae0acf447c02397342b6
cris <ctovar@abexuspr.com>
2023-04-06 05:37:04 -0500

    🫡 (0:12:16) fix my typo and render full background

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,8 @@
 const Auth = () => {
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')]"></div>
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+
+    </div>
   );
 }
 
