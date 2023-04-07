21b7cbff53209cd83a0bf47d68f9d060e9d8e0ad
cris <ctovar@abexuspr.com>
2023-04-07 05:44:30 -0500

    🍥 (1:33:42) login with google:
    to see it into mongo:
    use root;
    show collections;
    db.Account.find();
    db.User.find();

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -67,7 +67,7 @@ const Auth = () => {
                 <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                   <FaGithub size={30} />
                 </div>
-                <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <div onClick={() => signIn('google', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                   <FcGoogle size={30} />
                 </div>
               </div>
