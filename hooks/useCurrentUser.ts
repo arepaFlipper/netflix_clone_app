993fdd7ebf238c513cd6a33f2361c51e3483cfcd
cris <ctovar@abexuspr.com>
2023-04-07 08:53:41 -0500

    🎴 fix the useCurrentUser hook:
    i don't know what happened here

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,4 +1,4 @@
-import useSWR from 'swr';
+import useSwr from 'swr'
 
 import fetcher from '@/lib/fetcher';
 
@@ -6,5 +6,8 @@ const useCurrentUser = () => {
   const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
   return { data, error, isLoading, mutate };
 }
+  const response = useSwr('/api/current', fetcher);
+  return response
+};
 
 export default useCurrentUser;
