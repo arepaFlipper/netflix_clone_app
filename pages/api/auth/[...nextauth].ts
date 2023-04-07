3886b5c331533cf447365b3c5d574b069a86df69
cris <ctovar@abexuspr.com>
2023-04-07 05:29:37 -0500

    🚿  (1:29:45) login with github, to see it in mongo:
    use root;
    show collections;
    db.Account.find();
    db.User.find();

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,24 +2,35 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import prismadb from '@/lib/prismadb';
 import { compare } from 'bcrypt';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
     Credentials({
       id: 'credentials',
       name: 'Credentials',
       credentials: {
         email: {
           label: 'Email',
-          type:'text',
+          type: 'text',
         },
         password: {
           label: 'Password',
           type: 'password',
         }
       },
-      async authorize(credentials){
-        if(!credentials?.email || !credentials?.password){
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
           throw new Error('Email and password required');
         }
 
@@ -29,13 +40,13 @@ export default NextAuth({
           }
         });
 
-        if (!user || !user.hashedPassword){
+        if (!user || !user.hashedPassword) {
           throw new Error('Email does not exist');
         }
 
-        const isCorrectPassword = await compare(credentials.password,user.hashedPassword);
+        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
 
-        if(!isCorrectPassword){
+        if (!isCorrectPassword) {
           throw new Error('Incorrect password');
         }
 
@@ -47,10 +58,11 @@ export default NextAuth({
     signIn: '/auth',
   },
   debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prismadb),
   session: {
     strategy: 'jwt',
   },
-  jwt:{
+  jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
   secret: process.env.NEXTAUTH_SECRET,
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -65,11 +65,11 @@ const Auth = () => {
               <Input id="password" label="Password" type="password" onChange={(ev: React.BaseSyntheticEvent) => setPassword(ev.target.value)} value={password} />
               <button onClick={(isLogin) ? (login) : (register)} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{(isLogin) ? 'Login' : 'Sign up'}</button>
               <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                  <FcGoogle size={30} />
-                </div>
-                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                   <FaGithub size={30} />
+                </div>
+                <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <FcGoogle size={30} />
                 </div>
               </div>
               <p className="text-neutral-500 mt-12">
