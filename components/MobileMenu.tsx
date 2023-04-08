05edda9f0dbe2327b088ba940e7e1de0b84ab77f
cris <ctovar@abexuspr.com>
2023-04-08 06:43:32 -0500

    🐩 fix react key warnings

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -8,14 +8,14 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
   if (!visible) {
     return null
   }
-  const titles = ["Home", "Series", "Films", "New & Popular", "My List", "Browse by Language"];
+  const labels = ["Home", "Series", "Films", "New & Popular", "My List", "Browse by Language"];
   return (
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-4">
-        {titles.map((title) => {
+        {labels.map((label) => {
           return (
-            <div className="px-3 text-center text-white hover:underline">
-              {title}
+            <div key={label} className="px-3 text-center text-white hover:underline">
+              {label}
             </div>
           )
         })}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,15 +37,15 @@ const Navbar = () => {
     console.log(showMobileMenu); // DELETEME
   }, [showMobileMenu])
 
-  const titles = ["Home", "Series", "Films", "New & Popular", "My List", "Browse by Language"];
+  const labels = ["Home", "Series", "Films", "New & Popular", "My List", "Browse by Language"];
   return (
     <nav className="w-full fixed z-40">
       <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
-          {titles.map((title) => {
+          {labels.map((label) => {
             return (
-              <NavbarItem label={title} />
+              <NavbarItem key={label} label={label} />
             )
           })}
         </div>
