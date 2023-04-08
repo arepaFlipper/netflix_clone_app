057e6d084866947b5970874bb6bb269771f00689
cris <ctovar@abexuspr.com>
2023-04-08 06:43:32 -0500

    🐩 fix react key warnings

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -7,7 +7,7 @@ interface NavbarItemProps {
 
 const NavbarItem: React.FC<NavbarItemProps> = ({ label }) => {
   return (
-    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+    <div key={label} className="text-white cursor-pointer hover:text-gray-300 transition">
       {label}
     </div>
   )
