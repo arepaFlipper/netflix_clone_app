eb2fa4c7402e7032b1772a3817d0cd788581ad86
cris <ctovar@abexuspr.com>
2023-04-07 21:01:20 -0500

    💻 (2:19:00) target multiple groups with group/item

---
+++ components/AccountMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AccountMenu = () => {
+  return (
+    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="flex flex-col gap-3">
+        <div className="px-3 group/item flex flex-row gap-3 items-center w-full"></div>
+      </div>
+    </div>
+  )
+}
+
+export default AccountMenu
