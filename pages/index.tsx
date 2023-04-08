e4d572f085e5ec775c5b44707c8493af099946f8
cris <ctovar@abexuspr.com>
2023-04-07 19:59:32 -0500

    👘 (2:04:47) create the NavbarItem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
-import useCurrentUser from '@/hooks/useCurrentUser'
+import Navbar from "@/components/Navbar";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -18,14 +18,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-  console.log(`🦞%cindex.tsx:22 - user`, 'font-weight:bold; background:#5fa000;color:#fff;'); //DELETEME
-  console.log(user); // DELETEME
   return (
     <>
-      <h1 className="text-4xl text-green-500"> Netflix Clone </h1>
-      <p className="text-white">{`Logged in as:   ${user?.name} : ${user?.email}`}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+      <Navbar />
     </>
   )
 }
