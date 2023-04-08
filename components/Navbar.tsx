89af8f33269f7817ba11c2218775442b9cec9304
cris <ctovar@abexuspr.com>
2023-04-07 20:06:09 -0500

    🕦 (2:05:55) use NavbarItem as a cool component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
       <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
-          <NavbarItem />
+          <NavbarItem label="Home" />
         </div>
       </div>
     </nav>
