b7754a2370adb4ebdf9fefabf29253ced31f2e46
cris <ctovar@abexuspr.com>
2023-04-08 16:20:38 -0500

    💭 (3:40:15) put a router in play button

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { BsFillPlayFill } from 'react-icons/bs';
 import FavoriteButton from './FavoriteButton';
 
@@ -7,6 +8,7 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img src={data?.thumbnailUrl} className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]" alt='ThumbnailUrl' />
@@ -14,7 +16,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         <img src={data?.thumbnailUrl} alt="Thumbnail" className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]" />
         <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
           <div className="flex flex-row items-center gap-3">
-            <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300" onClick={() => { }}>
+            <div onClick={() => router.push(`/watch/${data?.id}`)} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300" >
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
