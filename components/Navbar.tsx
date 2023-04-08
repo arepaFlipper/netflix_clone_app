3354ce9d45cde4bd9ef5bc31a1711dbfe7b6f3b6
cris <ctovar@abexuspr.com>
2023-04-07 21:01:20 -0500

    💻 (2:19:00) target multiple groups with group/item

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from 'react';
 import NavbarItem from "./NavbarItem";
-import { BsChevronDown } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
 import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
@@ -24,6 +25,21 @@ const Navbar = () => {
           <p className="text-white text-sm">Browse</p>
           <BsChevronDown className="text-white transition" />
           <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsBell />
+          </div>
+          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="" />
+            </div>
+            <BsChevronDown className="text-white transition" />
+            <AccountMenu />
+          </div>
         </div>
       </div>
     </nav>
