d3683c244fcf75882446a0be326ae9398c8afe8e
cris <ctovar@abexuspr.com>
2023-04-07 20:36:05 -0500

    😂 me being funny with the code

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -5,21 +5,20 @@ interface MobileMenuProps {
 }
 
 const MobileMenu: React.FC<MobileMenuProps> = () => {
+  const titles = ["Home", "Series", "Films", "New & Popular", "My List", "Browse by Language"];
   return (
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-4">
-        <div className="px-3 text-center text-white hover:underline">
-          Home
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Series
-        </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Home
-        </div>
+        {titles.map((title) => {
+          return (
+            <div className="px-3 text-center text-white hover:underline">
+              {title}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
 }
 
-export default MobileMenu
+export default MobileMenu;
