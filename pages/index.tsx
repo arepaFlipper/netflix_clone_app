e681fbb30d9e995f11240ac6fae11be41abb7223
cris <ctovar@abexuspr.com>
2023-04-08 12:29:22 -0500

    🍲 (3:23:41)the favorites list into home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import { getSession } from "next-auth/react";
 import MovieList from "@/components/MovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,12 +23,16 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  console.log(`🚁%cindex.tsx:27 - favorites`, 'font-weight:bold; background:#6c9300;color:#fff;'); //DELETEME
+  console.log(favorites); // DELETEME
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   )
