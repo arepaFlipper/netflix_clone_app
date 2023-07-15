8d7da60aeb84a4b430b4753daa18238204d7ebba
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-14 20:09:32 -0500

    📊 (2:55:12) fetch data for the movieList

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { NextPageContext } from "next";
+import useMovieList from '@/hooks/useMovieList'
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import { getSession } from "next-auth/react";
+import MovieList from "@/components/MovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,10 +22,14 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
