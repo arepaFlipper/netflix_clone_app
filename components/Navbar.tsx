831680ae36360c21b51c6686923c1ae382a87d21
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-10-14 06:55:57 -0500

    🗝️ predeploy tests

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
   return (
     <nav className="w-full fixed z-40">
       <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
-        <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
+        <img className="h-4 lg:h-7" src="/images/metflix-logo.png" alt="logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
           {labels.map((label) => {
             return (
