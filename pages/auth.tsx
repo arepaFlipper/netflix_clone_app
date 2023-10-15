8ce3ebc16e5711f7c52d0e6de6647c07bef8dc1a
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-10-15 18:25:22 -0500

    🌁 add the getServerSideProps async function into auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,11 +1,27 @@
 import { useCallback, useState } from 'react';
 import axios from 'axios';
 import Input from "@/components/Input";
-import { signIn } from 'next-auth/react';
+import { signIn, getSession } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { useRouter } from 'next/router';
-import { errorMonitor } from 'events';
+
+import { NextPageContext } from 'next';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: {},
+  }
+}
 
 const Auth = () => {
   const router = useRouter();
